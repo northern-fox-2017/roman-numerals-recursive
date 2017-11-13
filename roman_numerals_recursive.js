f88f298496d0693e20eaabdc8707b0ca46e6e3b1
Fajar Patappari <top.sick.red@gmail.com>
2017-11-13 17:22:00 +0700

    Done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,48 @@
 function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+  var num = input
+  var str = ''
+  if (num === 0) {
+    return ''
+  }
+  if (num >= 1000) {
+    num -= 1000
+    str += 'M'
+  } else if (num >= 500 && num < 1000) {
+    num -= 500
+    str += 'D'
+  } else if (num >= 400 && num < 500) {
+    num -= 400
+    str += 'CD'
+  } else if (num >= 100 && num < 400) {
+    num -= 100
+    str += 'C'
+  } else if (num >= 90 && num < 100) {
+    num -= 90
+    str += 'XC'
+  } else if (num >= 50 && num < 90) {
+    num -= 50
+    str += 'L'
+  } else if (num >= 40 && num < 50) {
+    num -= 40
+    str += 'XL'
+  } else if (num >= 10 && num < 40) {
+    num -= 10
+    str += 'X'
+  } else if (num === 9) {
+    num -= 9
+    str += 'IX'
+  } else if (num >= 5 && num < 10) {
+    num -= 5
+    str += 'V'
+  } else if (num === 4) {
+    num -= 4
+    str += 'IV'
+  } else if (num >= 1 && num < 4) {
+    num -= 1
+    str += 'I'
+  }
+
+  return str + to_roman(num);
 }
 
 console.log('My totally sweet testing script for new roman\n')
