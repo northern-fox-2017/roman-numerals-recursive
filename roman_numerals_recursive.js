5ff6cb00a742f70f66b7f1c063374683da6e46ca
eksant <eksoct@yahoo.com>
2017-11-13 20:00:00 +0700

    Roman Numerals Recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function to_roman(input) {
-  // start your code here
-  return to_roman(input); 
+  var arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'XL', 'L', 'X', 'IX', 'V', 'IV', 'I'];
+  var res = '';
+  
+  if (input <= 0) {
+    return res;
+  } else {
+    for (var i = 0; i < arabic.length; i++) {
+      if (input % arabic[i] < input) {
+        res += roman[i];
+        input -= arabic[i];
+        return res + to_roman(input);
+      }
+    }
+  } 
 }
 
 console.log('My totally sweet testing script for new roman\n')
