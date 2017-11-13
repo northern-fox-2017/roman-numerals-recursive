4fcd5f607c7e1107dc19481d1003f1da3389851c
ariefmanda <arief.manda57@gmail.com>
2017-11-13 16:42:42 +0700

    Done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,51 @@
+
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  if (input===0){
+    return '';
+  }else{
+    if(input>1000){
+      input-=1000
+      roman ='M'
+    }else if(input>=900&&input<1000){
+      input-=900
+      roman ='CM'
+    }else if(input>=500&&input<900){
+      input-=500
+      roman ='D'
+    }else if(input>=400&&input<500){
+      input-=400
+      roman ='CD'
+    }else if(input>=100&&input<400){
+      input-=100
+      roman ='C'
+    }else if(input>=90&&input<100){
+      input-=90
+      roman ='XC'
+    }else if(input>=50&&input<90){
+      input-=50
+      roman ='L'
+    }else if(input>=40&&input<50){
+      input-=40
+      roman ='XL'
+    }else if(input>=10&&input<40){
+      input-=10
+      roman ='X'
+    }else  if(input>=9&&input<10){
+      input-=9
+      roman ='IX'
+    }else  if(input>=5&&input<9){
+      input-=5
+      roman ='V'
+    }else  if(input>=4&&input<5){
+      input-=4
+      roman ='IV'
+    }else  if(input>0&&input<4){
+      input-=1
+      roman ='I'
+    }
+    return roman + to_roman(input); 
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
