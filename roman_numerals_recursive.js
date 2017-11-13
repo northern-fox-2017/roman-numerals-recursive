f716bc3af3070036cd0f64ca1118d079e2d8a383
lenz77 <rickylenzun77@gmail.com>
2017-11-13 20:00:07 +0700

    Mission Completed

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,19 @@
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  var hasil = ""
+  var angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+  if(num < 1){
+    return hasil + "I"
+  } else{
+    for(var i = 0; i < angka.length; i++){
+      if(input < angka[i]){
+        hasil += roman[i]
+        input -= angka[i]
+      }
+    }
+  } return to_roman(input) + hasil 
 }
 
 console.log('My totally sweet testing script for new roman\n')
@@ -8,6 +21,6 @@ console.log('input | expected | actual')
 console.log('——————|——————————|————————')
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
-console.log('13    | XIII     | ', to_roman(13))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+// console.log('13    | XIII     | ', to_roman(13))
+// console.log('1453  | MCDLIII  | ', to_roman(1453))
+// console.log('1646  | MDCXLVI  | ', to_roman(1646))
