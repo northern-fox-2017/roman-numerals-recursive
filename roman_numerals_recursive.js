86067028b34376b5a671e66d080505589b9e3c13
aldiws <syldie.aldiwijaya@gmail.com>
2017-11-13 18:28:50 +0700

    roman-recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,18 @@
 function to_roman(input) {
   // start your code here
-  return to_roman(input); 
+  let decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  let roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+  for(let i=0;i<roman.length;i++){
+    if(input === 0){
+      return ''
+    }
+    if(input % decimal[i] < input){
+      input-=decimal[i]
+
+      return roman[i] + to_roman(input); 
+    }
+  }
+  
 }
 
 console.log('My totally sweet testing script for new roman\n')
